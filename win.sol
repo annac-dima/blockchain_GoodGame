c1c648b953a3154daccdcaffe4ac92fc453ce6a9
Francesca Bianchessi <62222730+francbianc@users.noreply.github.com>
2020-12-06 11:29:25 +0100

    Update win.sol

--- win.sol
+++ win.sol
@@ -7,26 +7,34 @@ import "https://github.com/francbianc/blockchain/blob/main/bet.sol";
 contract Win is Betting {
     // NB: inheritance therefore same balance 
     
-    //function vedo(uint _idBet) public view onlyOwner returns(address _a, bool _boola) {
-       // return (all_bets[_idBet].challenger, all_bets[_idBet].challenger_pay);
-    //}
+    // Set the commission fee of GoodGame (3%)
+    uint commission_cost = 3;
     
-    
+    // Only GG can transfer ETH from loser to winner and charge the commission fee
     function payOutChallenger(uint _idBet, bool _winnC, address payable _C, address payable _A) public onlyOwner returns(bool success, string memory name_winner) {
         require(all_bets[_idBet].challenger == _C && all_bets[_idBet].accepter == _A);
         require(all_bets[_idBet].challenger_pay == true && all_bets[_idBet].accepter_pay == true);
         
         uint256 prize = all_bets[_idBet].price_challenger.add(all_bets[_idBet].price_accepter);
+        uint256 commission = prize.mul(commission_cost)/100;
+        uint256 netPrize = prize.sub(commission);
+        
+        msg.sender.transfer(commission);
+
         if (_winnC == true) {
-            //all_bets[_idBet].challenger.send(prize);
-            _C.transfer(prize);
+            _C.transfer(netPrize);
             return (true, 'Challenger');} else {
-                //address payable winner = all_bets[_idBet].accepter;
-                _A.transfer(prize);
+                _A.transfer(netPrize);
                 return (true, 'Accepter');}
     }
     
+    // Double check to be sure the Inheritance has worked correctly 
     function getBalanceBet2() public view onlyOwner returns (uint) {
         return address(this).balance;}
+    
+    // GG may access its own balance 
+    function getGoodGameBalance() public view onlyOwner returns (uint256) {
+        return msg.sender.balance;
+    }
     
 }
