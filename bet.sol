2f6056a8235003bb67f83770ba7f88ed3ef4eeda
Francesca Bianchessi <62222730+francbianc@users.noreply.github.com>
2020-12-06 11:35:10 +0100

    Update bet.sol

--- bet.sol
+++ bet.sol
@@ -5,6 +5,7 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract Betting is Ownable {
     
+    // DECLARE VARIABLES  
     using SafeMath for uint;
     using SafeMath for int;
     using SafeMath for uint256;
@@ -24,7 +25,7 @@ contract Betting is Ownable {
         bool accepter_pay;
     }
 
-    // Save each Bet in a list 
+    // Save each Bet in a dictionary  
     mapping (uint => Bet) public all_bets;
       
     // How many bets so far? 
@@ -38,19 +39,15 @@ contract Betting is Ownable {
     
     
     // FUNCTIONS 
-    
-    
-    // Publish a new bet 
+    // Only GoodGame can publish a new bet (it is the first deployer of this contract) 
     function publishBet(uint _idC, uint _idA, address _challenge, address _accept, uint256 _priceC, uint256 _priceA, string memory _playerA, 
     string memory _playerC) public onlyOwner {
-        // Only GoodGame, the first deployer, can invoke this contract 
-        // Increase the bet counter
         betCounter++;
         
         // A and B must have enough funds in their wallets 
         require(_challenge.balance > _priceC && _accept.balance > _priceA);
     
-        // Store this bet into the contract
+        // Store this bet into the dictionary 
         all_bets[betCounter] = Bet(
             betCounter,
             _idC,
@@ -68,11 +65,7 @@ contract Betting is Ownable {
         emit LogPublishBet(betCounter, _challenge, _accept, _priceC, _priceA);
     }
     
-    
-    //function challengerTransfer() public view returns(address) {
-        //return msg.sender;
-    //}
-    
+    // B, the first challenger, must transfer ETH to this smart contract 
     function challengerTransfer(uint _idBet) public payable {
         require(msg.sender == all_bets[_idBet].challenger);
         if(msg.value != all_bets[_idBet].price_challenger) {
@@ -80,6 +73,7 @@ contract Betting is Ownable {
         } else {all_bets[_idBet].challenger_pay = true;}
     }
     
+    // A, the accepter, must transfer ETH to this smart contract 
     function accepterTransfer(uint _idBet) public payable {
         require(msg.sender == all_bets[_idBet].accepter);
         if(msg.value != all_bets[_idBet].price_accepter) {
@@ -87,12 +81,12 @@ contract Betting is Ownable {
         } else {all_bets[_idBet].accepter_pay = true;}
     }
     
-    // Only GoodGame can get this smart contract's balance
+    // Only GoodGame can check the balance of this smart contract 
     function getBalanceBet() public view onlyOwner returns (uint) {
         return address(this).balance;
     }
     
-    // Only GoodGame can get gamblers' balance
+    // Only GoodGame can check gamblers' balance
     function getBalanceGamblers(address _a, address _b) public view onlyOwner returns(uint256, uint256) {
         return (_a.balance, _b.balance);
     }
